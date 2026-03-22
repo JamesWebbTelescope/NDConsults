e51c2cb5425df3b9f410744336518b1d50e3832a
JamesWebbTelescope <Viktorfrom@gmail.com>
2026-03-22 10:49:59 +0100

    Rebuilt dist

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -49,7 +49,7 @@ export default function Navbar() {
 
   return (
     <nav className="flex gap-6 justify-center mt-6">
-      <a href="/RasbiTuts/" className={holoLinkClass}>
+      <a href="/NDConsults/" className={holoLinkClass}>
         Home
       </a>
     </nav>
