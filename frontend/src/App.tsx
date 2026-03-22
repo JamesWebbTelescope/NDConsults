654444f379d16347a7858daee84aa790cc5f97b2
JamesWebbTelescope <Viktorfrom@gmail.com>
2026-03-22 16:14:46 +0100

    Rebuitl dist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <HashRouter basename="/NDConsults/">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
