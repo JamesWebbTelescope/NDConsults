56ed6f13d476fd989c8a4c3e6c9a237465770075
JamesWebbTelescope <Viktorfrom@gmail.com>
2026-03-26 13:31:11 +0100

    Resized profile

--- frontend/src/components/Profiles.tsx
+++ frontend/src/components/Profiles.tsx
@@ -2,7 +2,7 @@ import ViktorFrom from '../assets/fil_002.jpg';
 
 export default function Profiles() {
     return (
-        <div className="p-4">
+        <div className="p-4 w-100 h-100 flex flex-col items-center justify-center">
             <h2 className="text-2xl font-bold mb-4">Profiles</h2>
             <img src={ViktorFrom} alt="Viktor From" className="w-auto h-auto square mb-4" />
             <p className="text-gray-300">Viktor From</p>
